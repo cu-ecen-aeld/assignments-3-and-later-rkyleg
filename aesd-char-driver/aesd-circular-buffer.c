9c1c76b1ff1e3dbe2696600b5b51356541b4f14a
Kyle Gay <kyle.gay@pm.me>
2024-03-03 18:31:49 -0600

    circular buffer implementation complete

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -8,6 +8,9 @@
  *
  */
 
+// #include <cstdint>
+#include <stdint.h>
+#include <stdio.h>
 #ifdef __KERNEL__
 #include <linux/string.h>
 #else
@@ -32,7 +35,37 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
     /**
     * TODO: implement per description
     */
+    printf("Char offset: %zu\n", char_offset);
+    printf("read out offset: %d\n", buffer->out_offs);
+    uint8_t cumulative_bytes = 0;
+    uint8_t previous_cumulative_bytes = 0;
+    uint8_t index;
+    struct aesd_buffer_entry *entry;
+    // AESD_CIRCULAR_BUFFER_FOREACH(entry,buffer,index){
+    for(index=buffer->out_offs,entry=&buffer->entry[index%AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED];
+        index<AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED+buffer->out_offs;
+        index++,entry=&buffer->entry[index%AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED]){
+      printf("Index: %d\n", index);
+      printf("Entry: %s\n", entry->buffptr);
+      previous_cumulative_bytes = cumulative_bytes;
+      cumulative_bytes += entry->size;
+      printf("Cumulative bytes: %d\n", cumulative_bytes);
+      if (char_offset < cumulative_bytes){
+        int byte_rtn = char_offset - previous_cumulative_bytes;
+        printf("char position: %d\n", byte_rtn);
+        if(char_offset > 0){
+          *entry_offset_byte_rtn = byte_rtn;
+        } else {
+          *entry_offset_byte_rtn = 0;
+        }
+        printf("Returning entry: %s\n", entry->buffptr);
+        return entry;
+      }
+    } 
+  
+    printf("No entry found\n");
     return NULL;
+  
 }
 
 /**
@@ -47,6 +80,39 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
     /**
     * TODO: implement per description
     */
+  buffer->in_offs = buffer->in_offs % 10;
+  buffer->out_offs = buffer->out_offs % 10;
+  printf("write in offs: %d\n", buffer->in_offs);
+  printf("read out offs: %d\n", buffer->out_offs);
+  printf("full: %d\n", buffer->full);
+  printf("write data: %s\n", add_entry->buffptr);
+
+  // buffer->in_offs = buffer->in_offs % 10;
+  buffer->entry[buffer->in_offs] = *add_entry;
+  printf("buff data: %s\n", buffer->entry[buffer->in_offs].buffptr);
+  if (buffer->in_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1){
+    // buffer->out_offs++;
+    buffer->full = true;
+  }
+  if ((buffer->in_offs == buffer->out_offs) && buffer->full){
+    buffer->out_offs++;
+    buffer->full = false;
+  }
+  buffer->in_offs++;
+  printf("write in offs: %d\n", buffer->in_offs);
+  printf("read out offs: %d\n", buffer->out_offs);
+  printf("------------------\n");
+  // if (buffer->in_offs == AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED - 1){
+  //   buffer->in_offs = 0;
+  //   buffer->full = true;
+  // } else {
+  //   buffer->in_offs++;
+  // }
+  // if (buffer->in_offs == buffer->out_offs){
+  //   if (buffer->full){
+  //     buffer->out_offs++;
+  //   }
+  // }
 }
 
 /**
